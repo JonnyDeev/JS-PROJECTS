0fdeaa31bd67c7efba9ba989df461790243c7196
JonnyDeev <jonnyp.dev@gmail.com>
2022-08-23 16:53:06 -0400

    First Upload

---
+++ Tip calculator/index.js
@@ -0,0 +1,8 @@
+const range = document.querySelector('#customRange1')
+const rangeDisplay = document.querySelector('#rangeValue')
+let rangeValue = range.value
+
+function updateRangeDisplay(value) {
+    rangeDisplay.value = value
+}
+
