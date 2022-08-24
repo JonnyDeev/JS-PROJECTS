df3893803b5fa123dd22c2d9b3f9025c23956ada
JonnyDeev <jonnyp.dev@gmail.com>
2022-08-24 13:19:23 -0400

    finished project(desktop version)

--- Tip calculator/index.js
+++ Tip calculator/index.js
@@ -1,8 +1,35 @@
-const range = document.querySelector('#customRange1')
-const rangeDisplay = document.querySelector('#rangeValue')
-let rangeValue = range.value
+const costOfService = document.querySelector('.costOfService')
+const rangeSlider = document.querySelector('#customRange1')
+const perInput = document.querySelector('#percentageInput')
+const tipInput = document.querySelector('#tip')
+const billInput = document.querySelector('#bill')
+// buttons
 
-function updateRangeDisplay(value) {
-    rangeDisplay.value = value
+const calculate = document.querySelector('#calculate')
+const reset = document.querySelector('#reset')
+
+calculate.addEventListener('click', () => {
+    let percentage = toNumber(perInput.value)
+    let cost = toNumber(costOfService.value)
+    let tip = (percentage/100) * cost
+    tipInput.value = tip
+    billInput.value = tip + cost
+    console.log(tip)
+})
+
+
+reset.addEventListener('click', () => {
+    rangeSlider.value = 15
+    perInput.value = 15
+    costOfService.value = 0
+    billInput.value = 0
+    tipInput.value = 0
+})
+
+
+
+function toNumber(string) {
+    let number = parseInt(string, 10)
+    return number
 }
 
